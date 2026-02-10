a778a47df8afdcf616eddfbcde816ebb72243a25
fyncootec <168513806+hemantabhai123@users.noreply.github.com>
2026-02-10 18:57:39 +0545

    Update

--- ebankingmgmt/urls.py
+++ ebankingmgmt/urls.py
@@ -5,4 +5,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
+    path('memberregister/', include('memberregister.urls')),
+    
 ]
